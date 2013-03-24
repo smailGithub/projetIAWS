2ab84277d4b3ea65d99aa5ca1af23a0ecbf1bced
Vincent Carassus <vincentcarassus@gmail.com>
2013-03-24 10:47:29 +0100

    Formattage + Correction bug

--- src/main/java/inscription/InscriptionImpl.java
+++ src/main/java/inscription/InscriptionImpl.java
@@ -13,45 +13,40 @@ import connexionDB.*;
 
 /**
  * @author Ismail
- *
+ * 
  */
-public class InscriptionImpl implements InscriptionInt{
-	
-	ConnectToDbInt request=new ConnectToDbImpl();
-	List<String> l=new ArrayList<String>();
+public class InscriptionImpl implements InscriptionInt {
+
+	ConnectToDbInt request = new ConnectToDbImpl();
+	List<String> l = new ArrayList<String>();
 	ResultSet res;
 	String requete;
 	String insert;
-	String db="applicationws";
+	String db = "applicationws";
 	int col;
-	
-	public int verificationPersonnel(String nom, String prenom, String mail,int adrPostale){
 
-		//Vérification du mail
-		if(mail.contains("@") && mail.substring(mail.lastIndexOf('@') + 1).equals("univ-tlse3.fr")){
-			//request.connexionDB(db);
-			ResultSet res=null;
-			requete="SELECT mail FROM personnel";
-			res=request.SelectRequest(requete,db);
-			//System.out.println(request.CoutResult(res));
-			l=request.CoutResult(res);
-			col=request.NbreCol(res);
+	public int verificationPersonnel(String nom, String prenom, String mail, int adrPostale) {
+
+		// Vérification du mail
+		if (mail.contains("@") && mail.substring(mail.lastIndexOf('@') + 1).equals("univ-tlse3.fr")) {
+			// request.connexionDB(db);
+			ResultSet res = null;
+			requete = "SELECT mail FROM personnel";
+			res = request.selectRequest(requete, db);
+			// System.out.println(request.CoutResult(res));
+			l = request.countResult(res);
+			col = request.nbCol(res);
 			System.out.println(col);
-			for(int i=0;i<l.size();i++){
-				//System.out.println(l.get(i));
-				if(l.get(i).equals(mail)){
+			for (int i = 0; i < l.size();i++) {
+				if (l.get(i).equals(mail))
 					return 100;
-				}
-				else{
-					insert="INSERT INTO personnel(`nom`,`prenom`,`mail`,`adrPostale`)VALUES('"+nom +"','"+prenom +"','"+mail +"',"+adrPostale +");";
-					request.InsertRequest(insert,db);
-					return 1;
-				}
 			}
+			insert = "INSERT INTO personnel(`nom`,`prenom`,`mail`,`adrPostale`)VALUES('" + nom + "','" + prenom
+					+ "','" + mail + "'," + adrPostale + ");";
+			request.insertRequest(insert, db);
+			return 0;
+			
 		}
-		else
-			return 110;
-		return 0;
+		return 110;
 	}
 }
-
